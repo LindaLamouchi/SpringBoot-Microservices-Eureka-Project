845d46954906c45dfd4255ab00279c39c8ee7d7e
AlDo <AlDo@linda.net>
2021-01-29 21:08:17 +0100

    first commit register service

---
+++ service-register/src/test/java/org/demo/ServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
