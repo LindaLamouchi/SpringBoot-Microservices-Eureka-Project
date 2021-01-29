76b6e5f1c97d5934ac864187d863de457331fe65
AlDo <AlDo@linda.net>
2021-01-29 21:07:48 +0100

    first commit service proxy

---
+++ service-proxy/src/test/java/org/demo/ServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
